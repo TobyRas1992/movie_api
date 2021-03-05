660fd5595674faffc5b56c917c9111131f5b448e
TobyRas1992 <tobiasrasmussen@outlook.com>
2021-03-05 12:35:41 +0100

    put JWT authentication back into 2 endpoints after updating the movie database

--- index.js
+++ index.js
@@ -140,9 +140,9 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (r
 
 
 // POST REQUESTS 
-//passport.authenticate('jwt', { session: false })
+
 //Adds new movie to to main movie list (updated)
-app.post('/movies', [check('Title', 'Title is required.').not().isEmpty(), check('Description', 'Description is required').not().isEmpty(), check('Actors', 'Actors need to be in an array.').isArray(), check('ImagePath', 'Correct image path required').isLength({min: 5}), check('Featured', 'Boolean value required.').isBoolean(), check('Released', 'Release year required').isLength({min: 4})], (req, res) =>{
+app.post('/movies', passport.authenticate('jwt', { session: false }), [check('Title', 'Title is required.').not().isEmpty(), check('Description', 'Description is required').not().isEmpty(), check('Actors', 'Actors need to be in an array.').isArray(), check('ImagePath', 'Correct image path required').isLength({min: 5}), check('Featured', 'Boolean value required.').isBoolean(), check('Released', 'Release year required').isLength({min: 4})], (req, res) =>{
     let errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array()});
@@ -240,9 +240,9 @@ app.delete('/users/byId/:_id', passport.authenticate("jwt", { session: false }),
             res.status(500).send('Error: ' + err);
         });
 });
-//passport.authenticate("jwt", { session: false }),
+//
 // Delete a movie by its title
-app.delete('/movies/:Title',  (req, res) => {
+app.delete('/movies/:Title', passport.authenticate("jwt", { session: false }), (req, res) => {
     Movies.findOneAndRemove({ Title: req.params.Title})
     .then((movie) => {
         if (!movie) {
